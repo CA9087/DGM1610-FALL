52846c810aa21b18d7b3eea22ff4283f939ef159
Connor <connorallred.13@gmail.com>
2019-08-26 17:25:13 -0600

    new files

---
+++ Variables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string Names = "Ehhhh";
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Names);    
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
