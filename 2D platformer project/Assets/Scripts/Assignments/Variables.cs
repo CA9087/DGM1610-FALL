df507bb79c9fee7e7d8a77bc1088931a98f0de63
Connor <connorallred.13@gmail.com>
2019-08-28 17:32:52 -0600

    Fixing issues

---
+++ 2D platformer project/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string firstName = "Connor";
+    int age = 19; 
+    float height = 5.75f; 
+    bool married = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("first Name: "+ firstName);   
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("married: " + married); 
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
