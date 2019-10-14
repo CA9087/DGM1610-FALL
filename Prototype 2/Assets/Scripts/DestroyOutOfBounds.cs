c15fb04f32db0608421eeeb265ed5a3a119b630c
Connor Allred <connorallred.13@gmail.com>
2019-10-13 21:35:32 -0600

    Finished Challenge 2

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -15,11 +15,11 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    if (transform.position.z > topBound)
+    if (transform.position.x > topBound)
     {
         Destroy(gameObject);    
     }    
-    else if (transform.position.z < lowerBound)
+    else if (transform.position.x < lowerBound)
     {
         Debug.Log("Game Over!");
         Destroy(gameObject);    
