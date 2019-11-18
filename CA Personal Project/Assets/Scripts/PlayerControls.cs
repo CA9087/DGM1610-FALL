6f3088fd9d66f27c984f19f648cc6c0fc30c2766
Connor Allred <connorallred.13@gmail.com>
2019-11-18 11:25:35 -0700

    Worked on personal project's player movement

---
+++ CA Personal Project/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float speed;
+    private float turnSpeed = 60f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        //this code makes the vehicle move
+        transform.Translate(Vector3.forward*Time.deltaTime * speed * ver);    
+        transform.Rotate(Vector3.up, turnSpeed * hor * Time.deltaTime);
+    }
+}
+    
